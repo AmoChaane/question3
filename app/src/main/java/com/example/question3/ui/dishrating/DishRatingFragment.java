8b6d084b0969dc10441fc3a6b15d4a7a372ecf2c
AmoChaaneQualtrix <amogelang.chaane@qualtrix.co.za>
2025-10-28 17:03:53 +0200

    initial commit

---
+++ app/src/main/java/com/example/question3/ui/dishrating/DishRatingFragment.java
@@ -0,0 +1,37 @@
+package com.example.question3.ui.dishrating;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.question3.databinding.FragmentHomeBinding;
+
+public class DishRatingFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DishRatingViewModel dishRatingViewModel =
+                new ViewModelProvider(this).get(DishRatingViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        dishRatingViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/example/question3/ui/restaurant/RestaurantFragment.java
@@ -0,0 +1,37 @@
+package com.example.question3.ui.restaurant;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.question3.databinding.FragmentHomeBinding;
+
+public class RestaurantFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        RestaurantViewModel restaurantViewModel =
+                new ViewModelProvider(this).get(RestaurantViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        restaurantViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
